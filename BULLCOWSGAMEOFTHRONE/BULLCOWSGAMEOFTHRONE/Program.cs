a21c53591c7e361f7a020bc788dcda0d91b3c121
SyMurrr <SyDFall@yandex.ru>
2021-01-25 19:41:54 +0300

    Получается блок схемка

---
+++ BULLCOWSGAMEOFTHRONE/BULLCOWSGAMEOFTHRONE/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BULLCOWSGAMEOFTHRONE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] GetRandomArray = new int[4];
+            GetRandomArray(GetRandomArray);
+        }
+
+        static int[] GetRandomArray(int[] array)
+        {
+
+        }
+    }
+}
